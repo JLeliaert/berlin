b68af7f1eea79b7bf7865f7e61f8aea9ce4b1587
JLeliaert <jonathan.leliaert@gmail.com>
2015-06-05 10:53:52 +0200

    added energy functions for particles

--- particle.go
+++ particle.go
@@ -12,3 +12,19 @@ type particle struct {
 	r      float64 // radius
 	msat   float64 // Saturation magnetisation in A/m
 }
+
+func (p particle) E_anis(theta float64) float64 {
+	return 0.
+}
+
+func (p particle) E_ext(theta float64) float64 {
+	return 0.
+}
+
+func (p particle) TS(theta float64) float64 {
+	return 0.
+}
+
+func (p particle) F(theta float64) float64 {
+	return p.E_anis(theta) + p.E_ext(theta) - p.TS(theta)
+}
