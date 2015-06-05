5ca353228e15abae0f8c3fbd5c2f657c7cf45604
JLeliaert <jonathan.leliaert@gmail.com>
2015-06-05 09:59:44 +0200

    added more files

---
+++ const.go
@@ -0,0 +1,40 @@
+//This file contains all the constants and init files
+
+package berlin
+
+import (
+	"fmt"
+	"math"
+)
+
+const (
+	gamma0 = 1.7595e11          // Gyromagnetic ratio of electron, in rad/Ts
+	mu0    = 4 * math.Pi * 1e-7 // Permeability of vacuum in Tm/A
+	muB    = 9.2740091523E-24   // Bohr magneton in J/T
+	kb     = 1.3806488E-23      // Boltzmann's constant in J/K
+	qe     = 1.60217646E-19     // Electron charge in C
+)
+
+func init() {
+
+	fmt.Println(`
+vinamax: a macrospin model to simulate magnetic nanoparticles
+Copyright (C) 2013  Jonathan Leliaert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see [http:fmt.Println(www.gnu.org/licenses/].
+
+contact: jonathan.leliaert@gmail.com
+
+
+`)
+}
---
+++ output.go
@@ -0,0 +1,43 @@
+package berlin
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+
+var (
+	outputFile     *os.File
+	err            error
+)
+
+//Initialise the outputdir
+func initOutput() {
+
+	// make and clear output directory
+	fname := os.Args[0]
+	f2name := strings.Split(fname, "/") // TODO: use path.Split?
+	outdir = fmt.Sprint(f2name[len(f2name)-1], ".out")
+	os.Mkdir(outdir, 0775)
+	dir, err3 := os.Open(outdir)
+	files, _ := dir.Readdir(1)
+	// clean output dir, copied from mumax
+	if len(files) != 0 {
+		filepath.Walk(outdir, func(path string, i os.FileInfo, err error) error {
+			if path != outdir {
+				os.RemoveAll(path)
+			}
+			return nil
+		})
+	}
+
+	if err3 != nil {
+		panic(err3)
+	}
+
+	outputFile, err = os.Create(outdir + "/table.txt")
+	check(err)
+
+}
